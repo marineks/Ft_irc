ed076bd33fa4ab0cb8f31b20cb48f235b43b0b6d
Marine SANJUAN <marine@sanjuan.eu>
2022-12-15 21:09:40 +0100

    some notes before coding

--- srcs/Commands.cpp
+++ srcs/Commands.cpp
@@ -10,5 +10,17 @@ Commands::~Commands()
 
 int	Commands::parseCommand(std::string cmd_line, cmd_struct &cmd_infos)
 {
+	std::cout << "This is the retrieved cmd_line : " << cmd_line << std::endl;
+	// exple de ligne : @id=234AB :dan!d@localhost PRIVMSG #chan :Hey what's up!
+	
+	// trouver le mot en uppercase
+	// tu boucles, des que tu trouves des majuscules tu les rentres dans une variable
+	// et à l'espace
+
+	// ce qui est devant : prefix
+
+
+	// ce qui va derriere : message
+
 	return (SUCCESS);
 }
