2b8055bb844f774bf8aa7d2a4f9e30c0f04d7c5f
Marine SANJUAN <marine@sanjuan.eu>
2023-01-09 17:31:36 +0100

    Small change to the accessors in Channel class : they return a ref of the object now

--- srcs/Channel.cpp
+++ srcs/Channel.cpp
@@ -21,11 +21,11 @@ Channel::~Channel() {}
 *				################################
 */
 
-std::string						Channel::getName() const 		{ return (_name); }
-std::string						Channel::getTopic() const 		{ return (_topic); }
-std::map <std::string, Client>&	Channel::getClientList()	 	{ return (_clientList); }
-std::vector<std::string>		Channel::getBannedUsers() const { return (_banned_users); }
-std::vector<std::string>		Channel::getOperators() const 	{ return (_operators); }
+std::string&						Channel::getName() 			{ return (_name); }
+std::string&						Channel::getTopic() 		{ return (_topic); }
+std::map <std::string, Client>&		Channel::getClientList()	{ return (_clientList); }
+std::vector<std::string>&			Channel::getBannedUsers()	{ return (_banned_users); }
+std::vector<std::string>&			Channel::getOperators() 	{ return (_operators); }
 
 void							Channel::setTopic(std::string newTopic)
 {
