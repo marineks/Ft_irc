15630db95c924d236d647c1a46f4cfd76aabfa5d
Marine SANJUAN <marine@sanjuan.eu>
2023-01-16 16:21:48 +0100

    Adding multiple channels done. Now must do: error handling, send info to Client, implement keys

--- srcs/commands/join.cpp
+++ srcs/commands/join.cpp
@@ -6,7 +6,6 @@ bool			containsAtLeastOneAlphaChar(std::string str);
 std::string		getChannelName(std::string msg_to_parse);
 void			addChannel(Server *server, std::string const &channelName);
 void			addClientToChannel(Server *server, std::string &channelName, Client &client);
-void			printChannel(Server *server, std::string &channelName);
 /**
  * @brief The JOIN command indicates that the client wants to join the given channel(s), 
  * 	each channel using the given key for it. The server receiving the command checks 
@@ -42,77 +41,62 @@ void			printChannel(Server *server, std::string &channelName);
  */
 void	join(Server *server, int const client_fd, cmd_struct cmd_infos)
 {
-	// Pour l'instant, on ne teste que les inputs faciles type "JOIN #foo"
-	std::string channelName = getChannelName(cmd_infos.message);
+	std::string channelName;
 
-	// Récupérer le Client client grace au client fd
-	std::map<const int, Client>	client_list = server->getClients();
-	std::map<const int, Client>::iterator it_client = client_list.find(client_fd);
-	Client client = it_client->second;
+	while (containsAtLeastOneAlphaChar(cmd_infos.message) == true)
+	{
+		channelName.clear();
+		channelName = getChannelName(cmd_infos.message);
 
-	// Récupérer le bon channel grâce au channel name
-	std::map<std::string, Channel>::iterator it = server->getChannels().find(channelName);
-	if (it == server->getChannels().end()) // si on ne le trouve pas, créer le channel
-		addChannel(server, channelName);
-		// server->addChannel(channelName);
-	
-	// vérifier si le client est banned avant de le join au channel
-	std::string client_nickname = client.getNickname();
-	std::map<std::string, Channel>::iterator it_chan = server->getChannels().find(channelName);
-	if (it_chan->second.isBanned(client_nickname) == true) {
-		std::cout << client.getNickname() << " is banned from " << channelName << std::endl; 
-		return ;
-	} 
-	else {
-		// addClientToChannel(server, channelName, client);
-		server->addClientToChannel(channelName, client);
-		it_chan->second.addFirstOperator(client.getNickname()); // FIXME: pourquoi le rajouter en oper par défaut au channel ?
-		printChannel(server, channelName);
+		// erase de la string le channel = "#foo,#bar" devient "#,#bar"
+		cmd_infos.message.erase(cmd_infos.message.find(channelName), channelName.length()); 
+		
+		// Récupérer le Client client grace au client fd
+		Client client = retrieveClient(server, client_fd);
+
+		// Récupérer le bon channel grâce au channel name
+		std::map<std::string, Channel>&			 channels 	= server->getChannels();
+		std::map<std::string, Channel>::iterator it			= channels.find(channelName);
+		if (it == channels.end()) // si on ne le trouve pas, créer le channel
+			addChannel(server, channelName);
+		
+		// vérifier si le client est banned avant de le join au channel
+		std::string client_nickname = client.getNickname();
+		std::map<std::string, Channel>::iterator it_chan = server->getChannels().find(channelName);
+		if (it_chan->second.isBanned(client_nickname) == true) {
+			std::cout << client.getNickname() << " is banned from " << channelName << std::endl; 
+			return ;
+		} 
+		else {
+			addClientToChannel(server, channelName, client);
+			// if le channel a pas d'operateur :
+			if (it_chan->second.getOperators().empty())
+				it_chan->second.addFirstOperator(client.getNickname());
+			
+		}
+		// TODO : prevoir check de la key; tester key et erase key du cmd_infos.msg
+		// TODO: Attention, cest pas fini quand qqun est add au chan, il y a plein d'infos à lui fournir
 	}
+	
 }
 
-// VERSION COMPLETE A TESTER SI LA VERSION BASIQUE MARCHE
-// Logique pour l'output 2 : on erase les channels (avec leur keys quand elles en ont) au fur et à mesure qu'on join
-// void	join(Server server, int const client_fd, cmd_struct cmd_infos)
-// {
-// 	std::string channelName;
+// While a client is joined to a channel, they receive all relevant information about 
+// that channel including the JOIN, PART, KICK, and MODE messages affecting the channel. 
+// They receive all PRIVMSG and NOTICE messages sent to the channel, and they also 
+// receive QUIT messages from other clients joined to the same channel (to let them 
+// know those users have left the channel and the network). 
 
-// 	while (containsAtLeastOneAlphaChar(cmd_infos.message) == false)
-// 	{
-// 		channelName.clear();
-// 		channelName = getChannelName(cmd_infos.message);
+// If a client’s JOIN command to the server is successful, the server MUST send, in this order:
 
-// 		// erase de la string le channel = "#foo,#bar" devient "#,#bar"
-// 		cmd_infos.message.erase(cmd_infos.message.find(channelName), channelName.length()); 
+// A JOIN message with the client as the message <source> and the channel they have 
+// joined as the first parameter of the message.
+// The channel’s topic (with RPL_TOPIC (332) and optionally RPL_TOPICWHOTIME (333)), 
+// and no message if the channel does not have a topic.
+// A list of users currently joined to the channel (with one or more RPL_NAMREPLY (353) 
+// numerics followed by a single RPL_ENDOFNAMES (366) numeric).
+// These RPL_NAMREPLY messages sent by the server MUST include the requesting client 
+// that has just joined the channel.
 
-// 		// Récupérer le Client client grace au client fd
-// 		std::map<const int, Client>	client_list = server.getClients();
-// 		std::map<const int, Client>::iterator it_client = client_list.find(client_fd);
-// 		Client client = it_client->second;
-
-// 		// Récupérer le bon channel grâce au channel name
-// 		std::map<std::string, Channel>			 channels = server.getChannels();
-// 		std::map<std::string, Channel>::iterator it = channels.find(channelName);
-// 		if (it == channels.end()) // si on ne le trouve pas, créer le channel
-// 			addChannel(server, channelName);
-		
-// 		// vérifier si le client est banned avant de le join au channel
-// 		std::string client_nickname = client.getNickname();
-// 		if (it->second.isBanned(client_nickname) == true) {
-// 			std::cout << client.getNickname() << " is banned from " << channelName << std::endl; 
-// 			return ;
-// 		} 
-// 		else {
-// 			addClientToChannel(server, channelName, client);
-// 			// if le channel a pas d'operateur :
-// 			if (it->second.getOperators().empty())
-// 				it->second.addFirstOperator(client.getNickname());
-// 		}
-// 		// TODO : prevoir check de la key; tester key et erase key du cmd_infos.msg
-// 		// TODO: Attention, cest pas fini quand qqun est add au chan, il y a plein d'infos à lui fournir
-// 		}
-	
-// }
 
 bool		containsAtLeastOneAlphaChar(std::string str)
 {
@@ -134,7 +118,7 @@ std::string	getChannelName(std::string msg_to_parse)
 	size_t i = 0;
 	while (!isalpha(msg_to_parse[i]))
 		i++;
-	while (isalpha(msg_to_parse[i])) // as soon as there is a space or comma, it means the word is finished
+	while (isalpha(msg_to_parse[i]) || msg_to_parse[i] == '-' || msg_to_parse[i] == '_') // as soon as there is a space or comma, it means the word is finished
 		channel_name += msg_to_parse[i++];
 	std::cout << "The channel name is : |" << channel_name << "|" << std::endl;
 	return (channel_name);
@@ -158,7 +142,6 @@ void	addClientToChannel(Server *server, std::string &channelName, Client &client
 {
 	std::map<std::string, Channel>::iterator it;
 	it = server->getChannels().find(channelName);
-	std::cout << "it->first = " << it->first << std::endl;
 	std::string client_nickname = client.getNickname();
 	if (it->second.doesClientExist(client_nickname) == false)
 	{
@@ -167,14 +150,4 @@ void	addClientToChannel(Server *server, std::string &channelName, Client &client
 	}
 	else 
 		std::cout << YELLOW << client.getNickname() << "already here\n" << RESET;
-}
-
-
-// NOTE: à qui sert cette fonction ? debug ?
-void	printChannel(Server *server, std::string &channelName)
-{
-	std::map<std::string, Channel>			 	channels = server->getChannels();
-	std::map<std::string, Channel> 				tmp; // NOTE: pourquoi un tmp ?
-	std::map<std::string, Channel>::iterator	it = channels.find(channelName);
-	it->second.printClientList();
 }
