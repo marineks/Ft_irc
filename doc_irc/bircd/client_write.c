4c221ea4e905fd28622cbb2d56d48045c5fa212a
Marine SANJUAN <marine@sanjuan.eu>
2023-01-20 17:48:28 +0100

    bit of cleaning

--- doc_irc/bircd/client_write.c
+++
@@ -1,7 +0,0 @@
-
-#include <sys/socket.h>
-#include "bircd.h"
-
-void	client_write(t_env *e, int cs)
-{
-}
--- doc_irc/bircd/do_select.c
+++
@@ -1,8 +0,0 @@
-
-#include <stdlib.h>
-#include "bircd.h"
-
-void	do_select(t_env *e)
-{
-  e->r = select(e->max + 1, &e->fd_read, &e->fd_write, NULL, NULL);
-}
