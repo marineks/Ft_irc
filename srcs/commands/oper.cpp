89f1a8ba87f1eaf8d123a43a563778cc79dd555e
Marine SANJUAN <marine@sanjuan.eu>
2023-01-23 21:21:47 +0100

    [OPER] Erased debug comments and first draft

--- srcs/commands/oper.cpp
+++ srcs/commands/oper.cpp
@@ -33,10 +33,6 @@ void oper(Server *server, int const client_fd, cmd_struct cmd_infos)
 	std::string name		= getName(cmd_infos.message);
 	std::string password	= getPassword(cmd_infos.message);
 
-	// DEBUG
-	std::cout << "Name: |" << name << "|" << std::endl;
-	std::cout << "Password: |" << password << "|" << std::endl;
-
 	if (name.empty() || password.empty())
 	{
 		sendServerRpl(client_fd, ERR_NEEDMOREPARAMS(client.getNickname(), cmd_infos.name));
@@ -109,24 +105,3 @@ static std::string	getPassword(std::string msg_to_parse)
 	}
 	return (password);
 }
-
-// 
-// 	std::string operatorName;
-// 	std::string password;
-
-// 	std::map<std::string, Channel> channels = server.getChannels();
-// 	std::map<std::string, Channel>::iterator it;
-// 	it = channels.find(channelName);
-
-// 	if (password != it.getOperatorPassword()) 
-// 	{
-// 		std::cout << "Wrong Password\n";
-// 		return;
-// 	}
-// 	if (it == channels.end())
-// 	{
-// 		std::cout << "That channel doesn't exist\n";
-// 		return ;
-// 	}
-// 	if (it->second.isOperator(operatorName) == false)
-// 		it->second.addFirstOperator(operatorName); // NOTE: refacto cette fonction qui fait comme AddOperator
