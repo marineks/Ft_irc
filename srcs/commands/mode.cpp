aaac8f38bd3d43fa34c1b6ceaab8c282e0aecac2
Tiffany Manolis <tiffany.mano@gmail.com>
2023-02-11 11:39:38 +0100

    REFACTO : remove unnecessary comments

--- srcs/commands/mode.cpp
+++ srcs/commands/mode.cpp
@@ -41,71 +41,10 @@
  * @param client_fd User sending a msg
  * @param cmd_infos Structure w/ prefix, command name and message
  * 
- * 
- * NUMERIC REPLIES:
- * User mode :
- * - ERR_NOSUCHNICK (401) : If <target> is a nickname that does not exist on the network,
- * - ERR_USERSDONTMATCH (502) : If <target> is a different nick than the user who sent the command
- * - RPL_UMODEIS (221) : If <modestring> is not given, the RPL_UMODEIS (221) numeric is sent back containing the current modes of the target user.
- * - ERR_UMODEUNKNOWNFLAG (501) : If one or more modes sent are not implemented on the server
- * 
- * 
- * Channel mode :
- * - ERR_NOSUCHCHANNEL (403) : If <target> is a channel that does not exist on the network
- * - RPL_CHANNELMODEIS (324) : If <modestring> is not given.
- * "<client> <channel> <modestring> <mode arguments>..." => Sent to a client to inform them of the currently-set modes of a channel. 
- * <channel> is the name of the channel.
- * <modestring> and <mode arguments> are a mode string and the mode arguments (delimited as separate parameters) as defined in the MODE message description.
- * Servers MAY choose to hide sensitive information such as channel keys when sending the current modes.
- * - ERR_CHANOPRIVSNEEDED (482) :  If a user does not have appropriate privileges to change modes on the target channel, the server MUST NOT process the message, and return this error
- * 
  * Useful link : https://modern.ircdocs.horse/#mode-message
  * https://www.techbull.com/techbull/guide/internet/irccommande.html
  * 
  */
-
-
-/*
-A CODER POUR l'USER : +i / +o
-- Invisible User Mode :
-This mode is standard, and the mode letter used for it is "+i".
-If a user is set to ‘invisible’, they will not show up in commands such as WHO or NAMES unless they share a channel with the user that submitted the command. 
-In addition, some servers hide all channels from the WHOIS reply of an invisible user they do not share with the user that submitted the command.
-
-- Oper User Mode :
-This mode is standard, and the mode letter used for is it "+o".
-If a user has this mode, this indicates that they are a network operator.
-
-
-A CODER POUR LE CHANNEL : +o / +k / +s / +t
-- Key Channel Mode :
-This mode is standard, and the mode letter used for it is "+k".
-This mode letter sets a ‘key’ that must be supplied in order to join this channel. 
-If this mode is set, its’ value is the key that is required. 
-Servers may validate the value (eg. to forbid spaces, as they make it harder to use the key in JOIN messages). 
-If the value is invalid, they SHOULD return ERR_INVALIDMODEPARAM. However, clients MUST be able to handle any of the following:
-
-    ERR_INVALIDMODEPARAM
-    ERR_INVALIDKEY
-    MODE echoed with a different key (eg. truncated or stripped of invalid characters)
-    the key changed ignored, and no MODE echoed if no other mode change was valid.
-
-If this mode is set on a channel, and a client sends a JOIN request for that channel, they must supply <key> in order for the command to succeed. 
-If they do not supply a <key>, or the key they supply does not match the value of this mode, they will receive an ERR_BADCHANNELKEY (475) reply and the command will fail.
-
-- Secret Channel Mode :
-This mode is standard, and the mode letter used for it is "+s".
-This channel mode controls whether the channel is ‘secret’, and does not have any value.
-A channel that is set to secret will not show up in responses to the LIST or NAMES command unless the client sending the command is joined to the channel. 
-Likewise, secret channels will not show up in the RPL_WHOISCHANNELS (319) numeric unless the user the numeric is being sent to is joined to that channel.
-
-- Protected Topic Mode :
-This mode is standard, and the mode letter used for it is "+t".
-This channel mode controls whether channel privileges are required to set the topic, and does not have any value.
-If this mode is enabled, users must have channel privileges such as halfop or operator status in order to change the topic of a channel. 
-In a channel that does not have this mode enabled, anyone may set the topic of the channel using the TOPIC command.
-
-*/
 
 void	broadcastToAllChannelMembers(Server *server, Channel &channel, std::string reply)
 {
@@ -330,15 +269,9 @@ void	modeFunction(Server *server, int const client_fd, cmd_struct cmd_infos)
 {
 	mode_struct	mode_infos;
 	
-	// std::cout << "\nMessage : |" << cmd_infos.message << "|" << std::endl;
 	cmd_infos.message.erase(0,1);
-	
 	// parse la commande
 	fillModeInfos(mode_infos, cmd_infos.message);
-	
-	// std::cout << "target : |" << mode_infos.target << "|" << std::endl;
-	// std::cout << "mode : |" << mode_infos.mode << "|" << std::endl;
-	// std::cout << "params : |" << mode_infos.params << "|" << std::endl;
 
 	if (mode_infos.target[0] == '#') // channel case
 		modeForChannel(server, mode_infos, client_fd);
