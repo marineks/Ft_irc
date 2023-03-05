402fb8215fe9e3a0e076219af59f1b117db85a22
Tiffany Manolis <tiffany.mano@gmail.com>
2023-03-05 15:10:23 +0100

    bot is coded with 4 interaction cmds : HELP, HOUR, LOVE and RANDOM

--- srcs/commands/bot.cpp
+++ srcs/commands/bot.cpp
@@ -14,21 +14,44 @@ static void	botHour(Server *server, int const client_fd, std::map<const int, Cli
 	addToClientBuffer(server, client_fd, RPL_PRIVMSG(bot, bot, it_client->second.getNickname(), time));
 }
 
+static void	botRandom(Server *server, int const client_fd, std::map<const int, Client>::iterator it_client, std::string bot)
+{
+	srand(time(NULL)); // initializes the random number generator with a seed value based on the current time
+    int index = rand() % 10 + 1; // number between 1 and 10
+	
+	std::string str;
+	switch (index)
+	{
+		case 1: str = "Wearing headphones for just an hour could increase the bacteria in your ear by 700 times."; break;
+		case 2: str = "Google images was literally created after Jennifer Lopez wore that infamous dress at the 2000 Grammys"; break;
+		case 3: str = "Los Angeles' full name is 'El Pueblo de Nuestra Senora la Reina de los Angeles de Porciuncula'"; break;
+		case 4: str = "Tigers have striped skin, not just striped fur."; break;
+		case 5: str = "Like fingerprints, everyone's tongue print is different."; break;
+		case 6: str = "Cat urine glows under a black-light."; break;
+		case 7: str = "A shrimp's heart is in its head."; break;
+		case 8: str = "The Spice Girls were originally a band called Touch."; break;
+		case 9: str = "The unicorn is the national animal of Scotland"; break;
+		case 10: str = "In 2014, there was a Tinder match in Antarctica"; break;
+	}
+	addToClientBuffer(server, client_fd, RPL_PRIVMSG(bot, bot, it_client->second.getNickname(), str));
+}
+
 void	bot(Server *server, int const client_fd, std::map<const int, Client>::iterator it_client, std::string bot_cmd)
 {
-   std::cout << "bot cmd : |" << bot_cmd << "|" << std::endl;
-   
-   std::string bot = "BOT";
-   
-   // change msg to send into caps
-	std::string	validCmds[3] = {
+	std::string bot = "BOT";
+	std::string	validCmds[4] = {
 		":HELP",
 		":HOUR",
+		":LOVE",
 		":RANDOM",
 		};
 
-   int index = 0;
-	while (index < 3)
+	// change bot_cmd into caps letter
+	for (size_t i = 0; i < bot_cmd.size(); i++)
+		bot_cmd[i] = std::toupper(bot_cmd[i]);
+	
+	int index = 0;
+	while (index < 4)
 	{
 		if (bot_cmd == validCmds[index])
 			break;
@@ -36,9 +59,10 @@ void	bot(Server *server, int const client_fd, std::map<const int, Client>::itera
 	}
 	switch (index + 1)
 	{
-		case 1: addToClientBuffer(server, client_fd, RPL_PRIVMSG(bot, bot, it_client->second.getNickname(), "Ask me 'HOUR' or 'RANDOM'")); break;
+		case 1: addToClientBuffer(server, client_fd, RPL_PRIVMSG(bot, bot, it_client->second.getNickname(), "Ask me 'HOUR', 'LOVE' or 'RANDOM'")); break;
 		case 2: botHour(server, client_fd, it_client, bot); break;
-		case 3: std::cout << "prout" << std::endl; break;
+		case 3: addToClientBuffer(server, client_fd, RPL_PRIVMSG(bot, bot, it_client->second.getNickname(), "dyoula, msanjuan, tmanolis and myself send you love through this terminal <3")); break;
+		case 4: botRandom(server, client_fd, it_client, bot); break;
 		default:
 			addToClientBuffer(server, client_fd, RPL_PRIVMSG(bot, bot, it_client->second.getNickname(), "Invalid request, ask 'HELP' for more infos"));
 	}
